4e9d8e2596101d350a224d6b97536cf3b86784fc
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-03 17:48:00 -0700

    working on the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,12 @@ class About extends Component {
         return (
             <div>
                 <h1>About</h1>
+
+                <div className="aboutPara">
+                    <p>
+                        I am a creative who is in love with creating things, whether it's original music, puzzles, or beautiful software applications... you name it I'll figure out how to make it! I've recently graduated from General Assembly: Full-Stack Software Engineer Immersive, where I focused on building full-stack applications using various technologies listed below and I want to help build your next big idea.
+                    </p>
+                </div>
             </div>
         );
     }
