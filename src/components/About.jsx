fa434df5251c56a326f7c9f6d57ad15cf69ce86b
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-04 20:18:52 -0700

    fixed the spacing issue b/w profile pic and about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,12 @@ class About extends Component {
     render() {
         return (
             <Container fluid>
-                <h1 className="display-1 float-left p-5">About</h1>
+                <h1 className="display-1 float-left p-5 maring: 5rem">About</h1>
 
-                <p className="aboutPara h6 p-5">
+                <img src={Picture} className="profilePic" />
+
+                <p className="aboutPara ">
                     I am a creative who is in love with creating things, whether it's original music, puzzles, or beautiful software applications... you name it I'll figure out how to make it! I've recently graduated from General Assembly: Full-Stack Software Engineer Immersive, where I focused on building full-stack applications using various technologies listed below and I want to help build your next big idea.
-                    <img src={Picture} className="profilePic float-mid" />
                 </p>
 
             </Container >
