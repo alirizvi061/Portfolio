c38f7b554d7d397c10f91bef65676245794303d9
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-08 14:54:16 -0700

    making css changes to the portfolio page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
-import Picture from "./profile-image.PNG"
+import React, { Component } from "react";
+import Picture from "./profile-image.PNG";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-
 
 class About extends Component {
-    render() {
-        return (
-            <Container>
-                <h1 className=" aboutTitle display-3 float-left p-5">BIO</h1>
+  render() {
+    return (
+      <Container>
+        <h1 className=" aboutTitle display-3 float-left p-5">BIO</h1>
 
-                <img src={Picture} className="profilePic" alt="profile" />
+        <img src={Picture} className="profilePic" alt="profile" />
 
-                <p className="aboutPara ">
-                    My name is Ali Rizvi, I am a creative who is in love with creating things, whether it's original music, puzzles, or beautiful software applications... you name it I'll figure out how to make it! I've recently graduated from General Assembly: Full-Stack Software Engineer Immersive, where I focused on building full-stack applications using various technologies listed below and I want to help build your next big idea.
-                </p>
-                <Link name='skills'>
-                </Link>
-                <div className="skillsDiv">
-                    <h1 className=" skillsTitle display-3 float-left p-5">Skills</h1>
-                    <div>
-                        <img src="HTML5_Logo.svg" className="language-thumbnail" alt="project thumbnail" /><br />
-                        <img src="CSS3_logo.svg" className="language-thumbnail" alt="project thumbnail" /><br />
-                        <img src="JS-logo.png" className="language-thumbnail" alt="project thumbnail" /><br />
-                    </div>
-                </div>
-            </Container >
-        );
-    }
+        <p className="aboutPara ">
+          My name is Ali Rizvi, I am a creative who is in love with creating
+          things, whether it's original music, puzzles, or beautiful software
+          applications... you name it I'll figure out how to make it! I've
+          recently graduated from General Assembly: Full-Stack Software Engineer
+          Immersive, where I focused on building full-stack applications using
+          various technologies listed below and I want to help build your next
+          big idea.
+        </p>
+      </Container>
+    );
+  }
 }
 
 export default About;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,56 +1,135 @@
-import React from 'react';
-import '../App.css'
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-
+import "../App.css";
 
 function Projects(props) {
-    const emoji = require("emoji-dictionary");
-    console.log(emoji.getName("😍"));
+  const emoji = require("emoji-dictionary");
+  console.log(emoji.getName("😍"));
 
-    return (
-        <>
-
-            <h1 className=" aboutTitle display-3 float-left p-5">Projects</h1><br />
-            <div className="projects">
-                <div className="flip-picture">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <img src="LYRA-Question.png" className="project-thumbnail" alt="project thumbnail" /><br />
-                        </div>
-                        <div className="flip-card-back">
-                            <p className="flip-card-back-text">The LYRA-Trivia project is a versitile single-page trivia game that pulls challenging questions from an API and scores user's responses! Click the 👉🏼 to go to the app or GitHub to see the code on GitHub! </p>
-                            <a href="https://alirizvi061.github.io/" target="_blank" rel="noopener noreferrer"> <span className="applink" role="img" aria-label="Link" >👉🏼</span> </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flip-picture">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <img src="rzlogin.png" className="project-thumbnail" alt="project thumbnail" />
-                        </div>
-                        <div className="flip-card-back">
-                            <p className="flip-card-back-text">RZEssentials is a community store that allows creatives to come together, list and buy items off the store! Click the 👉🏼 to go to the app or GitHub to see the code on GitHub! </p>
-                            <a href="https://rzessentials.herokuapp.com/sessions/new" target="_blank" rel="noopener noreferrer"> <span className="applink" role="img" aria-label="Link" >👉🏼</span> </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flip-picture">
-                    <div className="flip-card-inner">
-                        <div className="flip-card-front">
-                            <img src="SUDB-Main-WithMenu.png" className="project-thumbnail" alt="project thumbnail" />
-                        </div>
-                        <div className="flip-card-back">
-                            <p className="flip-card-back-text">The LYRA-Trivia project is a versitile single-page trivia game that pulls challenging questions from an API and scores user's responses! Click the 👉🏼 to go to the app or GitHub to see the code on GitHub! </p>
-                            <a href="https://sudb-front.herokuapp.com" target="_blank" rel="noopener noreferrer"> <span className="applink" role="img" aria-label="Link" >👉🏼</span> </a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <h1 className=" aboutTitle display-3 float-left p-5">Projects</h1>
+      <br />
+      <div className="projects">
+        <div className="flip-picture">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img
+                src="LYRA-Question.png"
+                className="project-thumbnail"
+                alt="project thumbnail"
+              />
+              <br />
             </div>
-        </>
-    )
+            <div className="flip-card-back">
+              <p className="flip-card-back-text">
+                The LYRA-Trivia project is a versitile single-page trivia game
+                that pulls challenging questions from an API and scores user's
+                responses! Click the 👉🏼 to go to the app or GitHub to see the
+                code on GitHub!{" "}
+              </p>
+              <a
+                href="https://alirizvi061.github.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <span className="applink" role="img" aria-label="Link">
+                  👉🏼
+                </span>{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="flip-picture">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img
+                src="rzlogin.png"
+                className="project-thumbnail"
+                alt="project thumbnail"
+              />
+            </div>
+            <div className="flip-card-back">
+              <p className="flip-card-back-text">
+                RZEssentials is a community store that allows creatives to come
+                together, list and buy items off the store! Click the 👉🏼 to go
+                to the app or GitHub to see the code on GitHub!{" "}
+              </p>
+              <a
+                href="https://rzessentials.herokuapp.com/sessions/new"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <span className="applink" role="img" aria-label="Link">
+                  👉🏼
+                </span>{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="flip-picture">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img
+                src="SUDB-Main-WithMenu.png"
+                className="project-thumbnail"
+                alt="project thumbnail"
+              />
+            </div>
+            <div className="flip-card-back">
+              <p className="flip-card-back-text">
+                The SUDB web application boasts a plethora of books using the
+                Google Books api and an amazing "Blind Date with a Book" feature
+                that allows the user to pick a random book of their favorite
+                category from New York Times - Best Seller's list.! Click the 👉🏼
+                to go to the app or GitHub to see the code on GitHub!{" "}
+              </p>
+              <a
+                href="https://sudb-front.herokuapp.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                <span className="applink" role="img" aria-label="Link">
+                  👉🏼
+                </span>{" "}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Link name="skills"></Link>
+      <div className="skillsDiv">
+        <h1 className=" skillsTitle display-3 float-left p-5">Skills</h1>
+      </div>
+      <br />
+      <div className="skillsPics">
+        <img
+          src="HTML5_Logo.svg"
+          className="language-thumbnail"
+          alt="project thumbnail"
+        />
+        <br />
+        <img
+          src="CSS3_logo.svg"
+          className="language-thumbnail"
+          alt="project thumbnail"
+        />
+        <br />
+        <img
+          src="JS-logo.png"
+          className="language-thumbnail"
+          alt="project thumbnail"
+        />
+        <br />
+      </div>
+    </>
+  );
 }
 
 export default Projects;
