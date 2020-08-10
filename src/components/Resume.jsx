6cefa8120653026a9b8aefb53c962680136aa11b
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-09 19:21:49 -0700

    working on media query

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,6 +3,7 @@ class Resume extends Component {
   render() {
     return (
       <div>
+        <h1 className=" resumeTitle display-3 float-left p-5">Resume</h1>
         <img className="resumePic" src="resume.png" alt="Resume" />
       </div>
     );
