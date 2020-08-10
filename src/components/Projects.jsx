97aa95b255438c3b8d805ce8917c716ece983531
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-10 11:18:08 -0700

    spaced the project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,128 +14,134 @@ function Projects(props) {
         <h1 className=" aboutTitle display-3 p-5 text-lg-left">Projects</h1>
         <br />
         <div className="projects">
-          <div className="flip-picture">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  src="LYRA-Question.png"
-                  className="project-thumbnail"
-                  alt="project thumbnail"
-                />
-                <br />
-              </div>
-              <div className="flip-card-back">
-                <p className="flip-card-back-text">
-                  The LYRA-Trivia project is a versitile single-page trivia game
-                  that pulls challenging questions from an API and scores user's
-                  responses! Click the 👉🏼 to go to the app or GitHub to see the
-                  code on GitHub!{" "}
-                </p>
-                <a
-                  href="https://github.com/alirizvi061/alirizvi061.github.io"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+          <div className="projectCardDiv">
+            <div className="flip-picture">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
                   <img
-                    className="gitLink"
-                    src="github-logo.png"
-                    alt="github logo"
-                  />{" "}
-                </a>
+                    src="LYRA-Question.png"
+                    className="project-thumbnail"
+                    alt="project thumbnail"
+                  />
+                  <br />
+                </div>
+                <div className="flip-card-back">
+                  <p className="flip-card-back-text">
+                    The LYRA-Trivia project is a versitile single-page trivia
+                    game that pulls challenging questions from an API and scores
+                    user's responses! Click the 👉🏼 to go to the app or GitHub to
+                    see the code on GitHub!{" "}
+                  </p>
+                  <a
+                    href="https://github.com/alirizvi061/alirizvi061.github.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="gitLink"
+                      src="github-logo.png"
+                      alt="github logo"
+                    />{" "}
+                  </a>
 
-                <a
-                  href="https://alirizvi061.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {" "}
-                  <span className="applink" role="img" aria-label="Link">
-                    👉🏼
-                  </span>{" "}
-                </a>
+                  <a
+                    href="https://alirizvi061.github.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <span className="applink" role="img" aria-label="Link">
+                      👉🏼
+                    </span>{" "}
+                  </a>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="flip-picture">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  src="rzlogin.png"
-                  className="project-thumbnail"
-                  alt="project thumbnail"
-                />
-              </div>
-              <div className="flip-card-back">
-                <p className="flip-card-back-text">
-                  RZEssentials is a community store that allows creatives to
-                  come together, list and buy items off the store! Click the 👉🏼
-                  to go to the app or GitHub to see the code on GitHub!{" "}
-                </p>
-                <a
-                  href="https://github.com/alirizvi061/rzessentials"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+          <div className="projectCardDiv">
+            <div className="flip-picture">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
                   <img
-                    className="gitLink"
-                    src="github-logo.png"
-                    alt="github logo"
-                  />{" "}
-                </a>
-                <a
-                  href="https://rzessentials.herokuapp.com/sessions/new"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {" "}
-                  <span className="applink" role="img" aria-label="Link">
-                    👉🏼
-                  </span>{" "}
-                </a>
+                    src="rzlogin.png"
+                    className="project-thumbnail"
+                    alt="project thumbnail"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p className="flip-card-back-text">
+                    RZEssentials is a community store that allows creatives to
+                    come together, list and buy items off the store! Click the
+                    👉🏼 to go to the app or GitHub to see the code on GitHub!{" "}
+                  </p>
+                  <a
+                    href="https://github.com/alirizvi061/rzessentials"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="gitLink"
+                      src="github-logo.png"
+                      alt="github logo"
+                    />{" "}
+                  </a>
+                  <a
+                    href="https://rzessentials.herokuapp.com/sessions/new"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <span className="applink" role="img" aria-label="Link">
+                      👉🏼
+                    </span>{" "}
+                  </a>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="flip-picture">
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <img
-                  src="SUDB-logo.png"
-                  className="project-thumbnail"
-                  alt="project thumbnail"
-                />
-              </div>
-              <div className="flip-card-back">
-                <p className="flip-card-back-text">
-                  The SUDB web application boasts a plethora of books using the
-                  Google Books api and an amazing "Blind Date with a Book"
-                  feature that allows the user to pick a random book of their
-                  favorite category from New York Times - Best Seller's list.!
-                  Click the 👉🏼 to go to the app or GitHub to see the code on
-                  GitHub!{" "}
-                </p>
-                <a
-                  href="https://github.com/root2point0/SUDB-front-end"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+          <div className="projectCardDiv">
+            <div className="flip-picture">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
                   <img
-                    className="gitLink"
-                    src="github-logo.png"
-                    alt="github logo"
-                  />{" "}
-                </a>
-                <a
-                  href="https://sudb-front.herokuapp.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {" "}
-                  <span className="applink" role="img" aria-label="Link">
-                    👉🏼
-                  </span>{" "}
-                </a>
+                    src="SUDB-logo.png"
+                    className="project-thumbnail"
+                    alt="project thumbnail"
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p className="flip-card-back-text">
+                    The SUDB web application boasts a plethora of books using
+                    the Google Books api and an amazing "Blind Date with a Book"
+                    feature that allows the user to pick a random book of their
+                    favorite category from New York Times - Best Seller's list.!
+                    Click the 👉🏼 to go to the app or GitHub to see the code on
+                    GitHub!{" "}
+                  </p>
+                  <a
+                    href="https://github.com/root2point0/SUDB-front-end"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="gitLink"
+                      src="github-logo.png"
+                      alt="github logo"
+                    />{" "}
+                  </a>
+                  <a
+                    href="https://sudb-front.herokuapp.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <span className="applink" role="img" aria-label="Link">
+                      👉🏼
+                    </span>{" "}
+                  </a>
+                </div>
               </div>
             </div>
           </div>
