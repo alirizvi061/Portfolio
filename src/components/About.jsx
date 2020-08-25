99fdc52d5f4b7fe9af7382e7c2589176d1b2a149
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-25 12:29:50 -0700

    added projects link to the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ class About extends Component {
             applications... you name it I'll figure out how to make it! I've
             recently graduated from General Assembly: Full-Stack Software
             Engineer Immersive, where I focused on building full-stack
-            applications using various technologies listed below and I want to
-            help build your next big idea.
+            applications using various technologies and I want to
+            help build your next big idea. Check out my <a href="/">Projects</a> page for more info!
           </p>
           <img src={Picture} className="profilePic" alt="profile" />
         </div>
