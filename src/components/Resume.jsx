5cd5ed9bb6ba49fff608981b138e798b2180c076
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-08-25 19:03:21 -0700

    added Creddle resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,9 @@ class Resume extends Component {
     return (
       <div>
         <h1 className=" resumeTitle display-3 p-5 text-lg-left">Resume</h1>
-        <img className="resumePic" src="resume.png" alt="Resume" />
+        {/* <img className="resumePic" src="resume.png" alt="Resume" /> */}
+        <iframe src="https://resume.creddle.io/resume/92ylnv5r0uo"
+          width="850" height="1100" seamless></iframe>
       </div>
     );
   }
