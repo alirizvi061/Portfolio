5baa2c64734118c03a4aeeb07f62716638dac00c
Ali Rizvi <alirizvi@Starboys-Mac.local>
2020-12-05 17:19:54 -0800

    fixing some warnings

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,9 +4,8 @@ class Resume extends Component {
     return (
       <div>
         <h1 className=" resumeTitle display-3 p-5 text-lg-left">Resume</h1>
-        {/* <img className="resumePic" src="resume.png" alt="Resume" /> */}
         <iframe src="https://resume.creddle.io/resume/92ylnv5r0uo"
-          width="850" height="1100" seamless></iframe>
+          width="850" height="1100" seamless title="Resume"></iframe>
       </div>
     );
   }
