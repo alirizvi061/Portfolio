93ddbd4e13aadd325666d40cf79ebea175d5910e
Ali Rizvi <alirizvi@Starboys-Mac.local>
2021-11-13 19:08:16 -0800

    Added effects to nave bar items

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,13 @@ class Navigation extends Component {
     render() {
         return (
             <div class="navigation">
-                <div></div>
+                <nav class="nav_links_div">
+                    <a href="" class="link_text">About</a>
+                    <a href="" class="link_text">My Skills</a>
+                    <a href="" class="link_text">Work</a>
+                    <a href="" class="link_text">Contact</a>
+                </nav>
+
             </div>
         )
     }
