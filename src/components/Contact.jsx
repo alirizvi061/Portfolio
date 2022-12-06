f44306ba7fa782f728d0083bc255d82093bf4a88
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-12-05 21:59:09 -0800

    Created error checking functionality for Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,51 +1,59 @@
 import React, { Component } from 'react'
-// import { init } from 'emailjs-com';
 import emailjs from 'emailjs-com';
 
 
 export default class Contact extends Component {
-    handleSubmit(event){
 
+    handleChange = (event) => {
+        if(event.target.value <= 0){
+            event.target.style.border = "1px solid red"
+        }else{
+            event.target.style.border = "none"
+        }
     }
- 
 
     render() {
 
-        let templateID = "template_n14srsh";
-        let serviceID = "service_79x9b9l"
-        let userID = "user_YIptbOHIy9sETu4BiabOb";
-
-
-      
         function sendEmail(event) {
-            // console.log(name.length);
             event.preventDefault();
+            
+            let submit = true;
+            let templateID = "template_n14srsh";
+            let serviceID = "service_79x9b9l"
+            let userID = "user_YIptbOHIy9sETu4BiabOb";
             let name = document.getElementById('Name');
             let email = document.getElementById('Email');
             let subject = document.getElementById('Subject');
             let message = document.getElementById('Message');
-            console.log(name.value.length)
+
             if(name.value.length <= 0){
-                alert("hi");
-            }else if (email.value.length <= 0){
-                alert("there")
-            }else if (subject.value.length <= 0){
-                alert("I'm")
-            }else if(message.value.length <= 0){
-                alert("Ali")
-            }else{
-                alert("submitting email")
+                name.style.border = "1px solid red"
+                submit = false;
+            }
+            if (email.value.length <= 0){
+                email.style.border = "1px solid red"
+                submit = false;
+            }
+            if (subject.value.length <= 0){
+                subject.style.border = "1px solid red"
+                submit = false;
+            }
+            if(message.value.length <= 0){
+                message.style.border = "1px solid red"
+                submit = false;
             }
             
-            
-            // emailjs.sendForm(serviceID, templateID, "#contactForm", userID)
-            //     .then((result) => {
-            //         console.log(result)
-            //         document.getElementById('contactForm').reset();
-            //         showThankYouMessage();
-            //     }, (error) => {
-            //         console.log(error)
-            //     });
+            if(submit){
+                alert("submitting email")
+                emailjs.sendForm(serviceID, templateID, "#contactForm", userID)
+                .then((result) => {
+                    console.log(result)
+                    document.getElementById('contactForm').reset();
+                    showThankYouMessage();
+                }, (error) => {
+                    console.log(error)
+                });
+            }
         }
 
         function showThankYouMessage(){
@@ -56,23 +64,20 @@ export default class Contact extends Component {
           
         }
 
-      
-        
-
         return (
             <div id="contactView" className='contactPage'>
                 <h1 className="contactTitle">Contact Me!</h1>
                 <div>
                     <form className="contact_form_class" id="contactForm" onSubmit={sendEmail}>
-                        <input className="contactTextBox" type="text" placeholder="Name" id="Name" name="Name" />
-                        <input className="contactTextBox" type="email" placeholder="Email" id="Email" name="Email" />
-                        <input className="contactTextBox" type="text" placeholder="Subject" id="Subject" name="Subject" />
-                        <textarea id="contact_message_area" type="text" placeholder="Message" id="Message" name="Message" ></textarea>
-                        <input type="Submit" value="Send Message" />
+                        <input onChange={(event) => this.handleChange(event)} className="contactTextBox" type="text" placeholder="Name" id="Name" name="Name" />
+                        <input onChange={(event) => this.handleChange(event)} className="contactTextBox" type="email" placeholder="Email" id="Email" name="Email" />
+                        <input onChange={(event) => this.handleChange(event)} className="contactTextBox" type="text" placeholder="Subject" id="Subject" name="Subject" />
+                        <textarea onChange={(event) => this.handleChange(event)} type="text" placeholder="Message" id="Message" name="Message" ></textarea>
+                        <input type="Submit" id="contactSubmitButton" value="Send Message" />
                     </form>
                 </div>
                 <span id="thankYouSpan">
-                    <div id="thankYoudiv">Thank you, We'll be in touch with you shortly!</div>
+                    <div id="thankYoudiv">Thank you, we'll be in touch with you shortly!</div>
                 </span>
             </div>
         )
