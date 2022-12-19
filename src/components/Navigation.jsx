bc327fe895c45cd2eac43f84aad3a92e1be51099
Ali Rizvi <alirizvi@Starboys-Mac.local>
2022-12-19 14:13:52 -0800

    Mobile css styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,6 +10,9 @@ class Navigation extends Component {
                         <img className="logo" src="Ali_logo.png" alt="Logo" />
                     </a>
                 </div><br />
+                <div>
+                    <img className="hmbrgr_icon" src="hamburger_menu_icon.png" alt="hamburger_icon" />
+                </div>
                 <nav className="nav_links_div">
                     <a href="/#aboutView" className="link_text">About</a>
                     <a href="/#projectView" className="link_text">Projects</a>
